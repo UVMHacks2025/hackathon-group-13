d932f3a05fe783d73f7d6edd93f54d09fcec5c2e
jklopson <jaklopson@gmail.com>
2025-03-01 12:01:26 -0500

    item inventory page

--- app.py
+++ app.py
@@ -6,9 +6,5 @@ app = Flask(__name__)
 def home():
     return render_template('item_inventory.html')
 
-@app.route("/subpage")
-def newpage():
-    return render_template('new_item.html')
-
 if __name__ =='__main__':
     app.run()
