306a1089c1a431c8973d91adf8bbdb92917e785a
jklopson <jaklopson@gmail.com>
2025-03-01 11:40:17 -0500

    item inventory page shell

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template('item_inventory.html')
+
+@app.route("/subpage")
+def newpage():
+    return render_template('new_item.html')
+
+if __name__ =='__main__':
+    app.run()
