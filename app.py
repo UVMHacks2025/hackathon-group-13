5d666a6c5c2d43b475dfc2fe3c5e522eade2e728
Aidan Hayes <aphayes@uvm.edu>
2025-03-01 12:16:38 -0500

    made basic changes

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def home():
+    if request.method == "POST":
+        test = request.form.get("test")
+        return "test is " + test 
     return render_template('item_inventory.html')
 
 if __name__ =='__main__':
